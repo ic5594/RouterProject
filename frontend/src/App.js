16a04dbfe557cfc9c49a7595d2b9d71879a2bb66
ic5594 <ic5594@naver.com>
2024-07-10 18:37:49 +0900

    router loader 기본 기능 커밋

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 import HomePage from './pages/HomePage';
 import Error from './pages/Error';
-import EventsPage from './pages/EventsPage'
+import Events, { loader as eventsLoader } from './pages/Events'
 import EventDetailPage from './pages/EventDetailPage'
 import NewEventPage from './pages/NewEventPage'
 import EditEventPage from './pages/EditEventPage'
@@ -12,14 +12,20 @@ import EventRoot from './pages/EventRoot';
 function App() {
   const router = createBrowserRouter([
     { 
-      path: '',
+      path: '/',
       element: <RootLayout />,
       error: <Error/>,
       children: [
         { index: true, element: <HomePage/> },
-        { path: 'events', element: <EventRoot/>,
+        { 
+          path: 'events', 
+          element: <EventRoot/>,
           children: [
-            { index: true, element: <EventsPage/> },
+            { 
+              index: true, 
+              element: <Events/>, 
+              loader: eventsLoader
+            },
             { path: ':id', element: <EventDetailPage/> },
             { path: 'new', element: <NewEventPage/> },
             { path: ':id/edit', element: <EditEventPage/> }
