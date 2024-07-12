565d0c90794ca828dbf6af97a0690a1e287d994c
ic5594 <ic5594@naver.com>
2024-07-12 15:32:49 +0900

    파일 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,53 +1,70 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
-import HomePage from './pages/HomePage';
-import Error from './pages/Error';
-import Events, { loader as eventsLoader } from './pages/Events'
-import EventDetailPage, { loader as eventDetailLoader,  action as eventDetailAction} from './pages/EventDetailPage'
-import NewEventPage, { action as newEventAcion} from './pages/NewEventPage'
-import EditEventPage from './pages/EditEventPage'
+import EditEventPage from './pages/EditEvent';
+import ErrorPage from './pages/Error';
+import EventDetailPage, {
+  loader as eventDetailLoader,
+  action as deleteEventAction,
+} from './pages/EventDetail';
+import EventsPage, { loader as eventsLoader } from './pages/Events';
+import EventsRootLayout from './pages/EventsRoot';
+import HomePage from './pages/Home';
+import NewEventPage from './pages/NewEvent';
 import RootLayout from './pages/Root';
-import EventRoot from './pages/EventRoot';
+import { action as manipulateEventAction } from './components/EventForm';
+import NewsletterPage, { action as newsletterAction } from './pages/Newsletter';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <HomePage /> },
+      {
+        path: 'events',
+        element: <EventsRootLayout />,
+        children: [
+          {
+            index: true,
+            element: <EventsPage />,
+            loader: eventsLoader,
+          },
+          {
+            path: ':eventId',
+            id: 'event-detail',
+            loader: eventDetailLoader,
+            children: [
+              {
+                index: true,
+                element: <EventDetailPage />,
+                action: deleteEventAction,
+              },
+              {
+                path: 'edit',
+                element: <EditEventPage />,
+                action: manipulateEventAction,
+              },
+            ],
+          },
+          {
+            path: 'new',
+            element: <NewEventPage />,
+            action: manipulateEventAction,
+          },
+        ],
+      },
+      {
+        path: 'newsletter',
+        element: <NewsletterPage />,
+        action: newsletterAction,
+      },
+    ],
+  },
+]);
 
 function App() {
-  const router = createBrowserRouter([
-    { 
-      path: '/',
-      element: <RootLayout />,
-      errorElement: <Error/>,
-      children: [
-        { index: true, element: <HomePage/> },
-        { 
-          path: 'events', 
-          element: <EventRoot/>,
-          children: [
-            { 
-              index: true, 
-              element: <Events/>, 
-              loader: eventsLoader
-            },
-            {
-              path: ':id',
-              loader: eventDetailLoader,
-              id: 'event-detail',
-              children: [
-                {
-                  index: true, 
-                  element: <EventDetailPage/>,
-                  action: eventDetailAction
-                },
-                { path: 'edit', element: <EditEventPage/> }
-              ]
-            },
-            { path: 'new', element: <NewEventPage/>, action: newEventAcion },
-          ]
-        },
-       
-      ]
-    },
-  ])
-
-  return <RouterProvider router={router}/>
+  return <RouterProvider router={router} />;
 }
 
 export default App;
